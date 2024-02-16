27a3e248211019934f84e0685ee1ebe1dd8e35d6
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-16 10:53:53 +0100

    Create HouseContext and HouseContextProvider components

--- src/components/CountryDropdown.js
+++ src/components/CountryDropdown.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { RiMapPinLine, RiArrowDownSLine, RiArrowUpSLine } from "react-icons/ri";
+import { Menu } from "@headlessui/react";
+import { HouseContext } from "./HouseContext";
 
 const CountryDropdown = () => {
   return <div>CountryDropdown</div>;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import CountryDropdown from "./CountryDropdown";
 import PropertyDropdown from "./PropertyDropdown";
 import PriceRangeDropdown from "./PriceRangeDropdown";
 import { RiSearch2Line } from "react-icons/ri";
+import { HouseContext } from "./HouseContext";
 
 const Search = () => {
   return (
